6e2152d651308827e8a0439ba2c0f65aef7c769e
Abbas Dargahi <adargahi97@gmail.com>
2022-04-15 16:23:38 -0500

    Assignment 9.4

--- counter.py
+++ counter.py
@@ -3,6 +3,25 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+
+    fromDict = {}
+
+    for line in handle:
+        line = line.strip()
+        if line.startswith("From "):
+            sender = line.split()[1]
+            if sender not in fromDict:
+                fromDict[sender] = 1
+            else:
+                fromDict[sender] += 1
+
+    dictVals = fromDict.values()
+    maxVal = max(dictVals)
+
+    print(max(fromDict, key=fromDict.get), maxVal)
+    
+
+
         
 ## if you want to test locally before you try to sync
 ## uncomment countEmail() and run > python counter.py
